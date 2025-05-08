2be547e25a44589b292967d86a2972e6d10e8045
Shiwani <shiwani12002@gmail.com>
2025-05-08 11:17:55 +0530

    WhiteNoise

--- api_app/settings.py
+++ api_app/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'silk.middleware.SilkyMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
