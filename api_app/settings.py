6c0f6476a82c2c5d61651688dfe5947423b863e6
Shiwani <shiwani12002@gmail.com>
2025-05-08 10:44:59 +0530

    Update settings.py

--- api_app/settings.py
+++ api_app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-xqpk6r29^su!5)5#*twilat1a!5itw^@@z)%t*od4rqa+hd8q1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['DRF_Project.onrender.com ', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -81,10 +84,10 @@ WSGI_APPLICATION = 'api_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
